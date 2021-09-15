6610d8054bcbaddd03885516c86b3d86cdcdb3f1
elserverhtml <53811726+elserverhtml@users.noreply.github.com>
2021-09-15 19:24:17 +0700

    test start

--- src/index.js
+++ src/index.js
@@ -1,17 +1,80 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function LessonButton(props) {
+    return (<button className="lessonButton"></button>)
+}
+
+
+function PersonCard(props) {
+    return (
+        <div></div>
+    )
+}
+
+class LessonCard extends React.Component {
+    getRequest() {
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', 'https://shrouded-journey-64297.herokuapp.com/index.php');
+        xhr.send({login: "еуче"});
+        xhr.onload = function() {
+            if (xhr.status !== 200) { // анализируем HTTP-статус ответа, если статус не 200, то произошла ошибка
+              alert(`Ошибка ${xhr.status}: ${xhr.statusText}`); // Например, 404: Not Found
+            } else { // если всё прошло гладко, выводим результат
+              alert(`Готово, получили ${xhr.responseText} байт`); // response -- это ответ сервера
+            }
+          };
+    }
+    render() {
+        return (
+            <div className="card" onClick = {() => this.getRequest()}>
+                <div className="card-text-block">
+                    <div className="card-text-main">{this.props.data[0]}</div>
+                    <div className="card-text-main">{this.props.data[1]}</div>
+                    <div className="card-text-second">{this.props.data[2]}</div>
+                    <div className="card-text-second">{this.props.data[3]}</div>
+                </div>
+                <div className="card-buttons">
+                    <LessonButton />
+                    <LessonButton />
+                    <LessonButton />
+                </div>
+            </div>
+        );
+    }
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            lessons : [["Математика 1.2", "12:40 - 13:30", "Устинова И.Г.", "к. 10 ауд. 412", {num:3, buttons:["1", "2", "3"] } ]],
+        }
+    }
+
+    cardClick() {
+        this.setState({lessons : this.state.lessons.concat([ "1" ]) });
+    }
+
+    render() {
+        return (
+            <div className="main-screen">
+                <div className="header" onClick = {() => this.cardClick()}>
+                    <PersonCard />
+                </div>
+                <div className="line" />
+                <div className="horizontal-container">
+                    {this.state.lessons.map( x => <LessonCard data = {x}/> )}
+                </div>
+            </div>
+        );
+    }
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
